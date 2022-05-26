9eb21956e0ebdb54dca963176a882dce0b41f059
Pete Ketcham <peokuk@gmail.com>
2022-05-25 21:48:09 -0500

    tweaks

--- docs/wordle.py
+++ docs/wordle.py
@@ -11,12 +11,12 @@ import re
 def main():
     """
     """
-    dict_file = "/usr/share/dict/words"
-    valid_wordlist = r"""^[a-z]{5}$"""
+    dict_file = "/usr/share/dict/words" # NOTE: MacOS dict location
+    
+    valid_wordlist = r"^[a-z]{5}$"
     with open(dict_file, 'r') as file:
-        words = file.readlines()
-    words = [word.rstrip() for word in words if re.search(valid_wordlist, word)]
-    # print(len(words))
+        words = [word.rstrip() for word in file if re.search(valid_wordlist, word)]
+    
     while True:
         guess = input("Word guess: ")
         result = input("results (g/y/b): ")
@@ -25,8 +25,10 @@ def main():
         
         for index, letter in enumerate(result):
             if letter == "g":
+                # Match words that have that exact letter in that exact spot
                 regex_is.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
             elif letter == "y":
+                # Match words that have that exact letter, but not in that spot
                 regex_is.append(fr"^.*[{guess[index]}].*")
                 regex_not.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
             else:
@@ -37,13 +39,13 @@ def main():
         for is_not in regex_not:
             words = [word for word in words if not re.search(is_not, word)]
             
-        if len(words) < 40:
+        if len(words) < 50:
             [print(word) for word in words]
         else:
             print(f"{len(words)} words remain")
         
         try:
-            _ = input("Press enter to continue, ctrl-c to exit")
+            _ = input("Press enter to continue, ctrl-c to exit ")
         except KeyboardInterrupt as e:
             quit()
             
