22880877d84c5b038cea1388cc340006cde2feed
Pete Ketcham <peokuk@gmail.com>
2022-05-26 08:04:37 -0500

    usability update

--- docs/wordle.py
+++ docs/wordle.py
@@ -17,37 +17,39 @@ def main():
     with open(dict_file, 'r') as file:
         words = [word.rstrip() for word in file if re.search(valid_wordlist, word)]
     
-    while True:
-        guess = input("Word guess: ")
-        result = input("results (g/y/b): ")
-        regex_is = []
-        regex_not = []
-        
-        for index, letter in enumerate(result):
-            if letter == "g":
-                # Match words that have that exact letter in that exact spot
-                regex_is.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
-            elif letter == "y":
-                # Match words that have that exact letter, but not in that spot
-                regex_is.append(fr"^.*[{guess[index]}].*")
-                regex_not.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
-            else:
-                regex_not.append(fr"{guess[index]}")
-        
-        for is_is in regex_is:
-            words = [word for word in words if re.search(is_is, word)]
-        for is_not in regex_not:
-            words = [word for word in words if not re.search(is_not, word)]
+    print("Guess word.  Results are g=green, y=yellow, b=black/no match")
+    _ = input("Press ctrl-c to exit at anytime")
+    
+    try:
+        while True:
+            guess = input("Word guess: ")
+            result = input("Results (g/y/b): ")
+            regex_is = []
+            regex_not = []
             
-        if len(words) < 50:
-            [print(word) for word in words]
-        else:
-            print(f"{len(words)} words remain")
-        
-        try:
-            _ = input("Press enter to continue, ctrl-c to exit ")
-        except KeyboardInterrupt as e:
-            quit()
+            for index, letter in enumerate(result):
+                if letter == "g":
+                    # Match words that have that exact letter in that exact spot
+                    regex_is.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
+                elif letter == "y":
+                    # Match words that have that exact letter, but not in that spot
+                    regex_is.append(fr"^.*[{guess[index]}].*")
+                    regex_not.append(r"."*index + fr"{guess[index]}" + r"."*(4 - index))
+                else:
+                    regex_not.append(fr"{guess[index]}")
+            
+            for is_is in regex_is:
+                words = [word for word in words if re.search(is_is, word)]
+            for is_not in regex_not:
+                words = [word for word in words if not re.search(is_not, word)]
+                
+            if len(words) < 50:
+                [print(word) for word in words]
+            else:
+                print(f"{len(words)} words remain")
+                
+    except KeyboardInterrupt as e:
+        quit()
             
 
 if __name__ == "__main__":
